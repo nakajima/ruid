3e80ed56721a82646c01e586f4e230927dc1beb3
Pat Nakajima <patnakajima@gmail.com>
2025-04-26 14:53:55 -0700

    init

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,57 @@
+use std::sync::Arc;
+
+use pixels::{Pixels, SurfaceTexture};
 use winit::application::ApplicationHandler;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::window::{Window, WindowId};
 
 #[derive(Default)]
-struct App {
-    window: Option<Window>,
+struct App<'a> {
+    window: Option<Arc<Window>>,
+    pixels: Option<Pixels<'a>>,
 }
 
-impl ApplicationHandler for App {
+impl<'a> ApplicationHandler for App<'a> {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        self.window = Some(
+        let window = Arc::new(
             event_loop
                 .create_window(Window::default_attributes())
                 .unwrap(),
         );
+
+        let window_size = window.inner_size();
+        let surface_texture =
+            SurfaceTexture::new(window_size.width, window_size.height, Arc::clone(&window));
+
+        // // Initialize pixels rendering
+        let pixels = Pixels::new(800, 600, surface_texture).unwrap();
+
+        self.pixels = Some(pixels);
+        self.window = Some(window);
+
+        self.window.as_ref().unwrap().request_redraw();
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {
+                // Notify that you're about to draw.
+                self.window.as_ref().unwrap().pre_present_notify();
+
+                let pixels = self.pixels.as_mut().unwrap();
+
+                let frame = pixels.frame_mut();
+                // Fill the frame with a solid color (RGBA)
+                for pixel in frame.chunks_exact_mut(4) {
+                    pixel.copy_from_slice(&[0x00, 0x80, 0x80, 0xFF]); // Teal color (R, G, B, A)
+                }
+                // Render the frame to the window
+                pixels.render().unwrap();
                 // Redraw the application.
                 //
                 // It's preferable for applications that do not render continuously to render in
@@ -55,7 +83,5 @@ fn main() {
     // This is ideal for non-game applications that only update in response to user
     // input, and uses significantly less power/CPU time than ControlFlow::Poll.
     event_loop.set_control_flow(ControlFlow::Wait);
-
-    let mut app = App::default();
-    event_loop.run_app(&mut app);
+    event_loop.run_app(&mut App::default()).unwrap();
 }
